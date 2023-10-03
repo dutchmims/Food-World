581ba176e5c62ac3356a0981a7ab11fb0caa1849
dutchmims <jameshealy901@gmail.com>
2023-10-03 15:57:07 +0000

    Add html and style

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Post, Tag
+from .models import Post, Tag  # Import the Tag model
 from .forms import CommentForm
 
 
