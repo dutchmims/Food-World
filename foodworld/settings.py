7613101106cdd41608a67c121acda382bb6d5f18
dutchmims <jameshealy901@gmail.com>
2023-11-01 23:16:33 +0000

    Create an ERD

--- foodworld/settings.py
+++ foodworld/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
+    'django_extensions',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
